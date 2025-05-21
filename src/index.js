4dc18b500a0fcac4922b1578318709024ac8194b
noahk <noah.kleinert@gmx.de>
2025-05-21 15:55:45 +0200

    Fix FCC test suite

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
       <App />
   </React.StrictMode>
 );
